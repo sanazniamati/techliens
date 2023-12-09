07b29fb230711b736967139b269c5face984ddc2
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-12-09 19:16:10 +0330

    feat:added ChakraProvider to main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <ChakraProvider>
-      <Router>
-        <Navbar />
-        <main></main>
-      </Router>
-    </ChakraProvider>
+    // <ChakraProvider>
+    <Router>
+      <Navbar />
+      <main></main>
+    </Router>
+    // </ChakraProvider>
   );
 }
 
