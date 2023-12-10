c3b20bba8aae9534b65601eb945bd6efcaac7a3b
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-12-10 09:07:57 +0330

    feat:added images and products

--- client/src/components/productsScreen.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function productsScreen(props) {
-  return <div></div>;
-}
-
-export default productsScreen;
---
+++ client/src/screens/productsScreen.jsx
@@ -0,0 +1,17 @@
+import {
+  Center,
+  Wrap,
+  WrapItem,
+  Spinner,
+  Stack,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  AlertTitle,
+} from "@chakra-ui/react";
+
+function productsScreen(props) {
+  return <Wrap spacing={10} justify={Center} minHeight={"100vh"}></Wrap>;
+}
+
+export default productsScreen;
